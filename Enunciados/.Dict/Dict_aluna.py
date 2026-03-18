eb7a9416c260dbfeac2669afe414f8cad1b78c0f
Tz  ๋ ࣭ ⭑ ⊹ <264721706+codartz@users.noreply.github.com>
2026-03-18 14:21:26 -0300

    Minha mensagem de commit

---
+++ Enunciados/.Dict/Dict_aluna.py
@@ -0,0 +1,10 @@
+Crie um dicionário aluna, contendo chave:valor para nome, idade e nota.
+
+Exiba o dicionário aluna utilizando print().
+
+Exiba o nome da aluna utilizando print(), acessando pela chave nome.
+
+Adicione uma nova chave, utilizando aluna["turma"] e depois exiba na tela o valor da nova chave.
+
+Atualize a nota utilizando aluna["nota"] e depois exiba na tela o valor da nova nota.
+
